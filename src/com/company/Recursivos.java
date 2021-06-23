01e918f6c171ec4fb11bcfbdf8730b64c7497392
Joao <joaofelipeschwab@gmail.com>
2021-06-22 23:17:19 -0300

    bora

---
+++ src/com/company/Recursivos.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Recursivos {
+
+    public class quickSort(int vetor[], int inicio, int fim){
+        if(vetor[].)
+
+    }
+}
+    def quickSort(vetor, inicio, fim):
+        if len(vetor) == 1:
+        return vetor
+
+        if inicio < fim:
+        indiceDaParticao = partition(vetor, inicio, fim)
+
+        quickSort(vetor, inicio, indiceDaParticao - 1)
+        quickSort(vetor, indiceDaParticao + 1, fim)
